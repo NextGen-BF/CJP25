9cf044caee3f3ef655a651af254503d8d2d9f56a
Rumen Yanchev <Rumen.Yanchev@Rumen-Yanchevs-Mac.local>
2025-01-23 15:32:10 +0200

    add Navbar component

---
+++ NextGen-BM-FE/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom"
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to='/'>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/login'>Log in</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
