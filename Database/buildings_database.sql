13c94d39874d56a8bc9c4fe47f5f93221a697190
IrinaAtanasova <irina.atanasova@blankfactor.com>
2025-01-24 17:28:58 +0200

    correcting default values and renaming ids

--- Database/buildings_database.sql
+++ Database/buildings_database.sql
@@ -1,153 +1,154 @@
 CREATE TABLE Enums (
-    EnumID INT PRIMARY KEY IDENTITY (1, 1),
+    EnumId INT PRIMARY KEY IDENTITY (1, 1),
     Description TEXT NOT NULL,
-    Title VARCHAR(40) UNIQUE NOT NULL,
-    Type VARCHAR(40) NOT NULL,
+    Title VARCHAR(50) UNIQUE NOT NULL,
+    Type VARCHAR(50) NOT NULL,
     Value INT,
 );
 
 CREATE TABLE Address (
-    AddressID INT PRIMARY KEY IDENTITY (1, 1),
+    AddressId INT PRIMARY KEY IDENTITY (1, 1),
     StreetNumber INT NOT NULL,
-    Entrance VARCHAR(255),
-    StreetName VARCHAR(255) NOT NULL,
-    District VARCHAR(255) NOT NULL,
-    PostalCode VARCHAR(255) NOT NULL,
-    Country VARCHAR(255) NOT NULL,
+    Entrance VARCHAR(5),
+    StreetName VARCHAR(50) NOT NULL,
+    District VARCHAR(50) NOT NULL,
+    PostalCode VARCHAR(12) NOT NULL,
+    Country VARCHAR(50) NOT NULL,
 );
 
 CREATE TABLE Buildings(
-    BuildingID INT PRIMARY KEY IDENTITY (1, 1),
-    AddressID INT UNIQUE NOT NULL, -- FK -> Address
-    Alias VARCHAR(255),
+    BuildingId INT PRIMARY KEY IDENTITY (1, 1),
+    AddressId INT UNIQUE NOT NULL, -- FK -> Address
+    Alias VARCHAR(50),
     FloorNum INT NOT NULL,
-    TotalBuildingSize DECIMAL(10, 2) NOT NULL,
+    TotalBuildingSize DECIMAL(6, 2) NOT NULL,
     DateBuilt DATE NOT NULL,
     NumOfElevators INT NOT NULL,
-    FOREIGN KEY (AddressID) REFERENCES Address(AddressID),
+    FOREIGN KEY (AddressId) REFERENCES Address(AddressId),
 );
 
 CREATE TABLE BuildingExpenses(
-    BuildingExpenseID INT PRIMARY KEY IDENTITY (1, 1),
-    BuildingID INT NOT NULL, -- FK -> Buildings
-    Title VARCHAR(255) NOT NULL,
-    TotalAmount DECIMAL(10, 2) NOT NULL,
-    SupplierID INT NOT NULL, -- FK -> Enums
+    BuildingExpenseId INT PRIMARY KEY IDENTITY (1, 1),
+    BuildingId INT NOT NULL, -- FK -> Buildings
+    Title VARCHAR(50) NOT NULL,
+    TotalAmount DECIMAL(6, 2) NOT NULL,
+    SupplierId INT NOT NULL, -- FK -> Enums
     DateOpened DATE NOT NULL,
     DueDate DATE NOT NULL,
     PaymentDate DATE NOT NULL,
     Description TEXT NOT NULL,
     IsTemplate BIT NOT NULL,
-    RepeatPeriodID INT NOT NULL, 
-    InvoiceURL TEXT NOT NULL,
-    FOREIGN KEY (BuildingID) REFERENCES Buildings(BuildingID),
-    FOREIGN KEY (SupplierID) REFERENCES Enums(EnumID),
+    RepeatPeriodId INT NOT NULL, 
+    InvoiceURL TEXT,
+    FOREIGN KEY (BuildingId) REFERENCES Buildings(BuildingId),
+    FOREIGN KEY (SupplierId) REFERENCES Enums(EnumId),
+    FOREIGN KEY (RepeatPeriodId) REFERENCES Enums(EnumId),
 );
 
 
 CREATE TABLE Property(
-    PropertyID INT PRIMARY KEY IDENTITY (1, 1),
-    BuildingID INT NOT NULL, --FK Buildings
+    PropertyId INT PRIMARY KEY IDENTITY (1, 1),
+    BuildingId INT NOT NULL, --FK Buildings
     PropertyNumber INT NOT NULL,
-    Size DECIMAL(10, 2) NOT NULL,
+    Size DECIMAL(6, 2) NOT NULL,
     Floor INT NOT NULL,
-    SizeOfIdealParts DECIMAL(10, 2) NOT NULL,
-    PropertyTypeID INT NOT NULL, -- FK Enums
+    SizeOfIdealParts DECIMAL(6, 2) NOT NULL,
+    PropertyTypeId INT NOT NULL, -- FK Enums
     EntranceIsExternal BIT NOT NULL,
-    FOREIGN KEY (BuildingID) REFERENCES Buildings(BuildingID),
-    FOREIGN KEY (PropertyTypeID) REFERENCES Enums(EnumID),
+    FOREIGN KEY (BuildingId) REFERENCES Buildings(BuildingId),
+    FOREIGN KEY (PropertyTypeId) REFERENCES Enums(EnumId),
 );
 
 CREATE TABLE PropertyResident(
-    PropertyResidentID INT PRIMARY KEY IDENTITY (1, 1),
-    FirstName VARCHAR(255) NOT NULL,
-    LastName VARCHAR(255) NOT NULL,
-    ResidentTypeID INT NOT NULL, -- FK -> Enums 
-    PropertyID INT NOT NULL, -- FK Property
+    PropertyResidentId INT PRIMARY KEY IDENTITY (1, 1),
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    ResidentTypeId INT NOT NULL, -- FK -> Enums 
+    PropertyId INT NOT NULL, -- FK Property
     EnterDate DATE DEFAULT GETDATE(),
     LeaveDate DATE,
-    FOREIGN KEY (ResidentTypeID) REFERENCES Enums(EnumID),
-    FOREIGN KEY (PropertyID) REFERENCES Property(PropertyID),
+    FOREIGN KEY (ResidentTypeId) REFERENCES Enums(EnumId),
+    FOREIGN KEY (PropertyId) REFERENCES Property(PropertyId),
 );
 
 CREATE TABLE PropertyExpenseTemplate (
-    PropertyExpenseTemplateID INT PRIMARY KEY IDENTITY (1, 1),
-    ExpenseTitle VARCHAR(40) NOT NULL,
+    PropertyExpenseTemplateId INT PRIMARY KEY IDENTITY (1, 1),
+    ExpenseTitle VARCHAR(50) NOT NULL,
     PerResident BIT NOT NULL,
-    RepeatPeriodID INT NOT NULL, --FK Enums
-    FOREIGN KEY (RepeatPeriodID) REFERENCES Enums(EnumID),
+    RepeatPeriodId INT NOT NULL, --FK Enums
+    FOREIGN KEY (RepeatPeriodId) REFERENCES Enums(EnumId),
 );
 
 CREATE TABLE PropertyExpense (
-    PropertyExpenseID INT PRIMARY KEY IDENTITY (1, 1),
-    PropertyExpenseTemplateID INT NOT NULL, -- FK  PropertyExpenseTemplate
-    RoleID INT, -- FK Roles
-    Price DECIMAL(10, 2) NOT NULL,
+    PropertyExpenseId INT PRIMARY KEY IDENTITY (1, 1),
+    PropertyExpenseTemplateId INT NOT NULL, -- FK  PropertyExpenseTemplate
+    RoleId INT, -- FK Roles
+    Price DECIMAL(6, 2) NOT NULL,
     StartDate DATE DEFAULT GETDATE(),
     EndDate DATE, -- nullable
     Description TEXT NOT NULL, 
-    FOREIGN KEY (PropertyExpenseTemplateID) REFERENCES PropertyExpenseTemplate(PropertyExpenseTemplateID),
+    FOREIGN KEY (PropertyExpenseTemplateId) REFERENCES PropertyExpenseTemplate(PropertyExpenseTemplateId),
 );
 
 
 CREATE TABLE PropertyPayments (
-    PaymentID INT PRIMARY KEY IDENTITY (1, 1),
-    PropertyID INT NOT NULL, -- FK Property
-    AmountOwed DECIMAL(10, 2) NOT NULL,
+    PaymentId INT PRIMARY KEY IDENTITY (1, 1),
+    PropertyId INT NOT NULL, -- FK Property
+    AmountOwed DECIMAL(6, 2) NOT NULL,
     DateOpened DATE DEFAULT GETDATE(),
     DueDate DATE NOT NULL,
-    PropertyExpenseID INT NOT NULL, --FK PropertyExpense
-    StatusID INT NOT NULL, -- FK -> Enums 
-    PaymentParentID INT, -- FK to the same table
-    PaymentMethodID INT NOT NULL, -- FK -> Enums 
-    FOREIGN KEY (PropertyID) REFERENCES Property(PropertyID),
-    FOREIGN KEY (PropertyExpenseID) REFERENCES PropertyExpense(PropertyExpenseID),
-    FOREIGN KEY (StatusID) REFERENCES Enums(EnumID),
-    FOREIGN KEY (PaymentParentID) REFERENCES PropertyPayments(PaymentID),
-    FOREIGN KEY (PaymentMethodID) REFERENCES Enums(EnumID),
+    PropertyExpenseId INT NOT NULL, --FK PropertyExpense
+    StatusId INT NOT NULL, -- FK -> Enums 
+    PaymentParentId INT, -- FK to the same table
+    PaymentMethodId INT NOT NULL, -- FK -> Enums 
+    FOREIGN KEY (PropertyId) REFERENCES Property(PropertyId),
+    FOREIGN KEY (PropertyExpenseId) REFERENCES PropertyExpense(PropertyExpenseId),
+    FOREIGN KEY (StatusId) REFERENCES Enums(EnumId),
+    FOREIGN KEY (PaymentParentId) REFERENCES PropertyPayments(PaymentId),
+    FOREIGN KEY (PaymentMethodId) REFERENCES Enums(EnumId),
 );
 
 
 CREATE TABLE UserBuildings (
-    UserBuildingsID INT PRIMARY KEY IDENTITY (1, 1),
-    UserID INT, -- FK Users
-    BuildingID INT NOT NULL, --FK Buildings
-    Approved BIT NOT NULL,
-    RoleID INT, -- FK Roles
+    UserBuildingsId INT PRIMARY KEY IDENTITY (1, 1),
+    UserId INT, -- FK Users
+    BuildingId INT NOT NULL, --FK Buildings
+    Approved BIT DEFAULT 0,
+    RoleId INT, -- FK Roles
     StartDate DATE DEFAULT GETDATE(),
     EndDate DATE,
-    FOREIGN KEY (BuildingID) REFERENCES Buildings(BuildingID),
+    FOREIGN KEY (BuildingId) REFERENCES Buildings(BuildingId),
 );
 
 CREATE TABLE PropertyUsers (
-    PropertyUsersID INT PRIMARY KEY IDENTITY (1, 1),
-    PropertyID INT NOT NULL, -- FK Property
-    UserID INT, -- FK Users
-    RoleID INT, -- FK Roles,
+    PropertyUsersId INT PRIMARY KEY IDENTITY (1, 1),
+    PropertyId INT NOT NULL, -- FK Property
+    UserId INT, -- FK Users
+    RoleId INT, -- FK Roles,
     EffectiveDate DATE DEFAULT GETDATE(),
     EndDate DATE,
-    PercentOfApartmentOwned DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (PropertyID) REFERENCES Property(PropertyID),
+    PercentOfApartmentOwned DECIMAL(6, 2) NOT NULL,
+    FOREIGN KEY (PropertyId) REFERENCES Property(PropertyId),
 );
 
 CREATE TABLE RepairRequests (
-    RequestID INT PRIMARY KEY IDENTITY (1, 1),
-    UserID INT, -- FK Users
+    RequestId INT PRIMARY KEY IDENTITY (1, 1),
+    UserId INT, -- FK Users
     RequestDescription TEXT NOT NULL,
-    RequestStatusID INT NOT NULL, -- FK Enums
+    RequestStatusId INT NOT NULL, -- FK Enums
     DateOpened DATE DEFAULT GETDATE(),
-    DateSettled DATE NOT NULL,
-    FOREIGN KEY (RequestStatusID) REFERENCES Enums(EnumID),
+    DateSettled DATE,
+    FOREIGN KEY (RequestStatusId) REFERENCES Enums(EnumId),
 );
 
 
 CREATE TABLE RequestNotes (
-    NoteID INT PRIMARY KEY IDENTITY (1, 1),
-    RequestID INT NOT NULL, -- FK RepairRequests
+    NoteId INT PRIMARY KEY IDENTITY (1, 1),
+    RequestId INT NOT NULL, -- FK RepairRequests
     NoteText TEXT NOT NULL, 
     CreateDate DATE DEFAULT GETDATE(),
     CreatedBy INT, -- FK Users
-    FOREIGN KEY (RequestID) REFERENCES RepairRequests(RequestID),
+    FOREIGN KEY (RequestId) REFERENCES RepairRequests(RequestId),
 );
 
 
