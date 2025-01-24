d56c68e6310265bec52c38cbace28394c3ed15fa
Rumen Yanchev <Rumen.Yanchev@Rumen-Yanchevs-Mac.local>
2025-01-24 15:33:14 +0200

    add routing for the new pages

--- NextGen-BM-FE/src/App.tsx
+++ NextGen-BM-FE/src/App.tsx
@@ -8,6 +8,9 @@ import MainLayout from "./layouts/MainLayout";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import { FC } from "react";
+import CreateBuildingPage from "./pages/CreateBuildingPage";
+import BuildingsListPage from "./pages/BuildingListPage";
+import BuildingPage from "./pages/BuildingPage";
 
 const App: FC = () => {
   const router = createBrowserRouter(
@@ -15,6 +18,9 @@ const App: FC = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/create/building" element={<CreateBuildingPage />} />
+        <Route path="/buildings" element={<BuildingsListPage />} />
+        <Route path="/building" element={<BuildingPage />} />
       </Route>,
     ),
   );
