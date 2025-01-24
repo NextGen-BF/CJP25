0cb7e478796ac485f2aea8cc672aa5cabe359a3a
Rumen Yanchev <Rumen.Yanchev@Rumen-Yanchevs-Mac.local>
2025-01-24 16:04:21 +0200

    add routes to the new pages

--- NextGen-BM-FE/src/App.tsx
+++ NextGen-BM-FE/src/App.tsx
@@ -8,6 +8,10 @@ import MainLayout from "./layouts/MainLayout";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import { FC } from "react";
+import CreateApartmentPage from "./pages/CreateApartmentPage";
+import ApartmentsListPage from "./pages/ApartmentsListPage";
+import ApartmentPage from "./pages/ApartmentPage";
+import ApartmentResidentsPage from "./pages/ApartmentResidentsPage";
 
 const App: FC = () => {
   const router = createBrowserRouter(
@@ -15,6 +19,10 @@ const App: FC = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/create/apartment" element={<CreateApartmentPage />} />
+        <Route path="/apartments" element={<ApartmentsListPage />} />
+        <Route path="/apartment" element={<ApartmentPage />} />
+        <Route path="/apartment/residents" element={<ApartmentResidentsPage />} />
       </Route>,
     ),
   );
