e372d32b0f46a16cbc941234f4d33686679e75be
Kiril Syuleyman <kiril.syuleyman@blankfactor.com>
2025-01-28 14:30:19 +0200

    Added signIn page using material ui

--- NextGen-BM-FE/src/pages/Account/LoginPage.tsx
+++ NextGen-BM-FE/src/pages/Account/LoginPage.tsx
@@ -1,6 +1,44 @@
 import { FC } from "react";
+import { AppProvider } from '@toolpad/core/AppProvider';
+import {
+  AuthResponse,
+  SignInPage,
+  type AuthProvider,
+} from '@toolpad/core/SignInPage';
+import { useTheme } from '@mui/material/styles';
+import { Link } from "@mui/material";
+
+const providers = [
+  {id: 'credentials', name: 'email and password'},
+  {id: 'google', name: 'Google'}
+]
+
+const signIn: (provider: AuthProvider) => void | Promise<AuthResponse> = async (
+  provider,
+) => {
+  const promise = new Promise<AuthResponse>((resolve) => {
+    setTimeout(() => {
+      console.log(`Sign in with ${provider.id}`);
+      resolve({ error: 'This is a fake error' });
+    }, 500);
+  });
+  return promise;
+};
+
+function SignUpLink() {
+  return (
+    <Link href="/signup" variant="body2">
+      Sign up
+    </Link>
+  );
+}
 
 const LoginPage: FC = () => {
-  return <h1>Log in Page</h1>;
+  const theme = useTheme();
+  return (
+    <AppProvider theme={theme}>
+      <SignInPage signIn={signIn} providers={providers} slots={{signUpLink: SignUpLink}}/>
+    </AppProvider>
+  );
 };
 export default LoginPage;
