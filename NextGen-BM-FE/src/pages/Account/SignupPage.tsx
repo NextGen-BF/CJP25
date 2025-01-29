1fccabcb7cf49d27ce80193251b09251df455b6d
Rumen Yanchev <Rumen.Yanchev@Rumen-Yanchevs-Mac.local>
2025-01-29 12:17:18 +0200

    add form and fields to SignupPage

--- NextGen-BM-FE/src/pages/Account/SignupPage.tsx
+++ NextGen-BM-FE/src/pages/Account/SignupPage.tsx
@@ -1,6 +1,89 @@
+import { useTheme } from "@mui/material/styles";
+import { Button, TextField } from "@mui/material";
+import { AppProvider } from "@toolpad/core";
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 const SignupPage: FC = () => {
-  return <h1>Sign up Page</h1>;
+  const theme = useTheme();
+
+  return (
+    <AppProvider theme={theme}>
+      <div>
+        <form>
+          <h1>Sign Up</h1>
+          <div>
+            <TextField
+              required
+              label="First Name"
+              helperText="Please provide a first name"
+              variant="outlined"
+              size="small"
+              sx={{ m: 1 }}
+            />
+            <TextField
+              required
+              label="Last Name"
+              helperText="Please provide a last name"
+              variant="outlined"
+              size="small"
+              sx={{ m: 1 }}
+            />
+          </div>
+
+          <TextField
+            required
+            label="Email"
+            type="email"
+            variant="outlined"
+            size="small"
+            fullWidth
+            sx={{ m: 1 }}
+          />
+
+          <TextField
+            required
+            label="Password"
+            type="password"
+            variant="outlined"
+            size="small"
+            fullWidth
+            sx={{ m: 1 }}
+          />
+
+          <TextField
+            required
+            label="Confirm Password"
+            type="password"
+            variant="outlined"
+            size="small"
+            fullWidth
+            sx={{ m: 1 }}
+          />
+
+          <TextField
+            label="Phone number"
+            type="number"
+            variant="outlined"
+            size="small"
+            fullWidth
+            sx={{ m: 1 }}
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ m: 1 }}
+          >
+            Submit
+          </Button>
+
+          <Link to="/login">Already Have an Account</Link>
+        </form>
+      </div>
+    </AppProvider>
+  );
 };
 export default SignupPage;
