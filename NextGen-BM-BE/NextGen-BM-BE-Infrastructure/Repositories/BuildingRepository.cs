71440517caa5c9b16e450365ec86f47f2308f040
Kiril Syuleyman <kiril.syuleyman@blankfactor.com>
2025-01-29 11:31:17 +0200

    Linked up controller with service and repo. Will add use cases later

--- NextGen-BM-BE/NextGen-BM-BE-Infrastructure/Repositories/BuildingRepository.cs
+++ NextGen-BM-BE/NextGen-BM-BE-Infrastructure/Repositories/BuildingRepository.cs
@@ -1,33 +1,59 @@
+using Microsoft.EntityFrameworkCore;
 using NextGen_BM_BE_Domain.Entities.BuildingAggregate;
 using NextGen_BM_BE_Domain.Interfaces;
+
+
 namespace NextGen_BM_BE_Infrastructure.Repositories{
 
     public class BuildingRepository : IBuildingRepository
     {
+        private readonly DataContext dbContext;
+        public BuildingRepository(DataContext _dbContext)
+        {   
+            dbContext = _dbContext;
+        }
         public async Task CreateBuildingAsync(Building building)
         {
-            throw new NotImplementedException();
+            await dbContext.Buildings.AddAsync(building);
+            await dbContext.SaveChangesAsync();        
         }
 
         public async Task DeleteBuildingAsync(int buildingId)
         {
-            throw new NotImplementedException();
+            Building? buildingToDelete = await this.GetBuildingByIdAsync(buildingId);
+            if (buildingToDelete is not null)
+            {
+                dbContext.Buildings.Remove(buildingToDelete);
+                await dbContext.SaveChangesAsync();
+            }
         }
 
         public async Task<List<Building>> GetAllBuildingsAsync()
         {
-            throw new NotImplementedException();
+            return await dbContext.Buildings.ToListAsync();
         }
 
         public async Task<Building> GetBuildingByIdAsync(int buildingId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Building? foundBuilding = await dbContext.Buildings.FindAsync(buildingId);
+                if (foundBuilding is null)
+                {
+                    throw new KeyNotFoundException("The building was not found.");
+                }
+                return foundBuilding;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("GetBuildingByIdAsync threw an error of: ", ex);
+            }
         }
 
         public async Task UpdateBuildingAsync(Building building)
         {
-            throw new NotImplementedException();
-        }
+            dbContext.Buildings.Update(building);
+            await dbContext.SaveChangesAsync();        }
     }
 }
 ;
