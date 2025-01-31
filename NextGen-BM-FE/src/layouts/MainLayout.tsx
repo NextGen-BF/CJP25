546d45567f06271238b9a97759fa803ba10aeed1
Anton Kostov <anton.kostov@blankfactor.com>
2025-01-31 18:06:01 +0200

    putting nav into a drawer

--- NextGen-BM-FE/src/layouts/MainLayout.tsx
+++ NextGen-BM-FE/src/layouts/MainLayout.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import { Box } from "@mui/material";
 
 const MainLayout: FC = () => {
   return (
-    <>
+    <Box sx={{ display: 'flex' }}>
       <Navbar />
       <Outlet />
-    </>
+    </Box>
   );
 };
 export default MainLayout;
