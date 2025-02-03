8376cee2d4cc2603382ee63e1e9c8d87dac4f37d
Anton Kostov <anton.kostov@blankfactor.com>
2025-02-03 14:35:40 +0200

    restructured nav and grouped links into accordion menus

--- NextGen-BM-FE/src/components/Navbar.tsx
+++ NextGen-BM-FE/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { Breadcrumbs, Button, Drawer, Menu, MenuItem, MenuList } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Breadcrumbs, Button, Drawer, Menu, MenuItem, MenuList } from "@mui/material";
 
 
 const Navbar: FC = () => {
@@ -14,60 +14,89 @@ const Navbar: FC = () => {
     variant="permanent">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/login">Log in</NavLink>
-      <Button>Requests</Button>
-      <MenuList>
-          <MenuItem>
-            <NavLink to="/create/request">Create Request</NavLink>
-          </MenuItem>
-          <MenuItem>
-            <NavLink to="/requests">Requests List</NavLink>
-          </MenuItem>
-          <MenuItem>
-            <NavLink to="/request">Request Details</NavLink>
-          </MenuItem>
-      </MenuList>
-      <Button>Payments</Button>
-      <MenuList>
-        <MenuItem>
-          <NavLink to="/payment">Payment</NavLink>
-          <NavLink to="/property/fees">property Fees</NavLink>
-        </MenuItem>
-      </MenuList>
-      <Button>Properties</Button>
-      <MenuList>
-        <MenuItem>
-          <NavLink to="/properties">properties List</NavLink>
-        </MenuItem>
-        <MenuItem>
-          <NavLink to="/property">property Details</NavLink>
-        </MenuItem>
-        <MenuItem>
-          <NavLink to="/create/property">Create property</NavLink>
-        </MenuItem>
-        <MenuItem>
-          <NavLink to="/property/residents">property Residents</NavLink>
-        </MenuItem>
-        <MenuItem>
-        </MenuItem>
-      </MenuList>
+      
+      <Accordion>
+        <AccordionSummary>
+          <Button>Requests</Button>
+        </AccordionSummary>
+        <AccordionDetails>
+          <MenuList>
+            <MenuItem>
+              <NavLink to="/create/request">Create Request</NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to="/requests">Requests List</NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to="/request">Request Details</NavLink>
+            </MenuItem>
+          </MenuList>
+        </AccordionDetails>  
+      </Accordion>  
+      <Accordion>
+        <AccordionSummary>
+          <Button>Payments</Button>
+        </AccordionSummary>
+        <AccordionDetails>
+          <MenuList>
+            <MenuItem>
+              <NavLink to="/payment">Payment</NavLink>
+              <NavLink to="/property/fees">property Fees</NavLink>
+            </MenuItem>
+          </MenuList>
+        </AccordionDetails>
+      </Accordion>  
+      <Accordion>
+        <AccordionSummary>
+          <Button>Properties</Button>
+        </AccordionSummary>
+        <AccordionDetails> 
+          <MenuList>
+            <MenuItem>
+              <NavLink to="/properties">properties List</NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to="/property">property Details</NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to="/create/property">Create property</NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to="/property/residents">property Residents</NavLink>
+            </MenuItem>
+            <MenuItem>
+            </MenuItem>
+          </MenuList>
+        </AccordionDetails> 
+      </Accordion>
+      
+      
+      <Accordion>
+        <AccordionSummary> 
+          <Button>Building</Button>
+        </AccordionSummary>   
+        <AccordionDetails>  
+          <MenuList>
+            <MenuItem>
+              <NavLink to="/building">Building Details</NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to="/create/building">Create Building</NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to="/buildings">Buildings List</NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink to="/building/expenses">Building Expenses</NavLink>
+            </MenuItem>
+          </MenuList>
+        </AccordionDetails> 
+      </Accordion>
       <NavLink to="/signup">Sign up</NavLink>
       <NavLink to="/recover">Recover Account</NavLink>
-      <NavLink to="/account">My Account</NavLink>
-      <Button>Building</Button>
-      <MenuList>
-        <MenuItem>
-          <NavLink to="/building">Building Details</NavLink>
-        </MenuItem>
-        <MenuItem>
-          <NavLink to="/create/building">Create Building</NavLink>
-        </MenuItem>
-        <MenuItem>
-          <NavLink to="/buildings">Buildings List</NavLink>
-        </MenuItem>
-        <MenuItem>
-          <NavLink to="/building/expenses">Building Expenses</NavLink>
-        </MenuItem>
-      </MenuList>
+      <Button>
+        <NavLink to="/account">My Account</NavLink>
+      </Button>
       <Breadcrumbs>
       </Breadcrumbs>  
     </Drawer>
