01b5e8ea9e188bc500230dd7b3a08cbbb15d585f
Anton Kostov <anton.kostov@blankfactor.com>
2025-02-03 13:12:03 +0200

    return createdataction on new property endpoint

--- NextGen-BM-BE/NextGen-BM-BE-API/Controllers/PropertyController.cs
+++ NextGen-BM-BE/NextGen-BM-BE-API/Controllers/PropertyController.cs
@@ -56,7 +56,7 @@ public class PropertyController: ControllerBase {
     [Route("new")]
     public async Task<IActionResult> CreateProperty(Property property){
         await _propertyService.CreatePropertyAsync(property);
-        return Ok();
+        return CreatedAtAction(nameof(GetPropertyById), property);
     }
 
     [HttpPut]
