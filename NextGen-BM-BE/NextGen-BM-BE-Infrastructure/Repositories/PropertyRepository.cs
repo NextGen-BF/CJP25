43b000a77a7c832411d50a4ab130bc01e7cfec9f
Anton Kostov <anton.kostov@blankfactor.com>
2025-02-03 10:17:22 +0200

    wrap repoository operations in trycatch

--- NextGen-BM-BE/NextGen-BM-BE-Infrastructure/Repositories/PropertyRepository.cs
+++ NextGen-BM-BE/NextGen-BM-BE-Infrastructure/Repositories/PropertyRepository.cs
@@ -1,3 +1,4 @@
+using System.Data.Common;
 using Microsoft.EntityFrameworkCore;
 using NextGen_BM_BE_Domain.Entities.PropertyAggregate;
 using NextGen_BM_BE_Domain.Interfaces;
@@ -13,29 +14,69 @@ namespace NextGen_BM_BE_Infrastructure.Repositories{
         }
         public async Task CreatePropertyAsync(Property property)
         {
-            await _dataContext.Properties.AddAsync(property);
-            await _dataContext.SaveChangesAsync();
+            try
+            {
+                await _dataContext.Properties.AddAsync(property);
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (DbException exception)
+            {
+                //will eventually be replaced by custom one
+                throw new Exception("Couldn't retrieve datafor this property");
+            }
         }
 
         public async Task DeletePropertyAsync(int propertyId)
         {
-            await _dataContext.Properties.Where(p=>p.PropertyId==propertyId).ExecuteDeleteAsync();
+            try
+            {
+                await _dataContext.Properties.Where(p=>p.PropertyId==propertyId).ExecuteDeleteAsync();
+            }
+            catch (DbException exception)
+            {
+                //will eventually be replaced by custom one
+                throw new Exception("Couldn't delete this property");
+            }
         }
 
         public async Task<List<Property>> GetAllPropertiesAsync()
         {
-            return await _dataContext.Properties.ToListAsync();
+            try 
+            {
+                return await _dataContext.Properties.ToListAsync();
+            }
+            catch (DbException exception)
+            {
+                //will eventually be replaced by custom one
+                throw new Exception("Couldn't retrieve data for the properties");
+            }
         }
 
         public async Task<List<Property>> GetPropertiesByBuildingIdAsync(int buildingId)
         {
-            var properties=await _dataContext.Properties.Where(p=>p.BuildingId==buildingId).ToListAsync();
-            return properties;
+            try 
+            {
+                var properties=await _dataContext.Properties.Where(p=>p.BuildingId==buildingId).ToListAsync();
+                return properties;
+            }
+            catch (DbException exception)
+            {
+                //will eventually be replaced by custom one
+                throw new Exception("Couldn't retrieve datafor this building's properties");
+            }
         }
 
         public async Task<List<Property>> GetPropertiesByUserIdAsync(int userId)
         {
-            return await _dataContext.Properties.Where(p=>p.Users.Any(u=>u.UserId==userId)).ToListAsync();
+            try 
+            {
+                return await _dataContext.Properties.Where(p=>p.Users.Any(u=>u.UserId==userId)).ToListAsync();
+            }
+            catch (DbException exception)
+            {
+                //will eventually be replaced by custom one
+                throw new Exception("Couldn't retrieve data for this user's properties");
+            }
         }
 
         public async Task<Property> GetPropertyByIdAsync(int propertyId)
@@ -45,8 +86,16 @@ namespace NextGen_BM_BE_Infrastructure.Repositories{
 
         public async Task UpdatePropertyAsync(Property property)
         {
-            _dataContext.Properties.Update(property);
-            await _dataContext.SaveChangesAsync();
+            try 
+            {
+                _dataContext.Properties.Update(property);
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                //will eventually be replaced by custom one
+                throw new Exception("Property couldn't be updated with the new data");
+            }
         }
     }
 
