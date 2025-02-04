144b1c71883c07c7efb06b4522d1f32567555d0f
Kiril Syuleyman <kiril.syuleyman@blankfactor.com>
2025-02-04 16:39:54 +0200

    Added more stuff to property table

--- NextGen-BM-FE/src/pages/Buildings/Create/CreateBuildingPage.tsx
+++ NextGen-BM-FE/src/pages/Buildings/Create/CreateBuildingPage.tsx
@@ -4,22 +4,31 @@ import { Address, Building } from "../../../models/building";
 import { useAppDispatch } from "../../../redux/store";
 import { createBuilding } from "../../../redux/services/buildingService";
 import { createBuildingConstants } from "../../../constants/constants";
-import "./createBuilding.scss"
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import "./createBuilding.scss";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { Property } from "../../../models/property";
 
-
-
 const CreateBuildingPage: FC = () => {
-  const [rows, setRows] = useState<Property[]>([]);
+  const [rows, setRows] = useState<Property[]>([{
+        propertyId: 0,
+        propertyNumber: 1,
+        buildingId: 1,
+        size: 1,
+        floor: 1,
+        sizeOfIdealParts: 1,
+        entranceIsExternal: false,
+        propertyExpenses: null,
+        payments: null,
+        residentHistory: null
+  }]);
   const columns: GridColDef<(typeof rows)[number]>[] = [
-    {field: 'propertyId', headerName: 'ID', width: 50},
-    {field: 'propertyNumber', headerName: 'Property Number', width: 150},
-    {field: 'size', headerName: 'Property Size', width: 150},
-    {field: 'floor', headerName: 'Property Floor', width: 150},
-    {field: 'sizeOfIdealParts', headerName: 'Ideal Parts', width: 150},
-    {field: 'entranceIsExternal', headerName: 'External Entrance', width: 150},
-  ]
+    {field: "id", headerName: "ID", width: 70},
+    { field: "propertyNumber", headerName: "Property Number", width: 150, type:'number', editable: true },
+    { field: "size", headerName: "Property Size", width: 150, editable: true, type: 'number'},
+    { field: "floor", headerName: "Property Floor", width: 150, editable: true, type:'number' },
+    { field: "sizeOfIdealParts", headerName: "Ideal Parts", width: 150, editable: true, type: 'number' },
+    {field: "entranceIsExternal", headerName: "External Entrance", width: 150,editable: true, type: 'boolean'},
+  ];
   const dispatch = useAppDispatch();
   const [address, setAddress] = useState<Address>({
     streetName: "",
@@ -39,7 +48,7 @@ const CreateBuildingPage: FC = () => {
     dateBuilt: new Date(),
     numOfElevators: 1,
     buildingExpenses: null,
-    buildingProperties: null,
+    buildingProperties: rows,
   });
 
   const handleAddressChange = (
@@ -47,7 +56,7 @@ const CreateBuildingPage: FC = () => {
   ) => {
     const { name, value } = e.target;
     setAddress((previousData) => ({ ...previousData, [name]: value }));
-    setFormData((previousData)=> ({ ...previousData, address: address }));
+    setFormData((previousData) => ({ ...previousData, address: address }));
   };
 
   const handleChange = (
@@ -57,135 +66,161 @@ const CreateBuildingPage: FC = () => {
     setFormData((previousData) => ({ ...previousData, [name]: value }));
   };
 
-  const handleCreateBuildingSubmission = (event: React.FormEvent<HTMLFormElement>) => {
+  const handlePropertyChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    setRows((previousData) => ({ ...previousData, [name]: value }));
+    console.log(rows)
+    setFormData((previousData) => ({ ...previousData, [name]: value }));
+  };
+
+  const handleCreateBuildingSubmission = (
+    event: React.FormEvent<HTMLFormElement>,
+  ) => {
     event.preventDefault();
     dispatch(createBuilding(formData));
-  }
+  };
 
   return (
     <div>
-    <div className="text-field-container">
-    <form className="create-building-form" onSubmit={(e) => handleCreateBuildingSubmission(e)}>
-        <h1 className="create-building-header">{createBuildingConstants.createHeader}</h1>
-        <TextField
-          name="alias"
-          label="Building Alias"
-          type="text"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleChange(e)}
-        />
-        <TextField
-          name="floorNum"
-          label="Amount of Building Floors"
-          type="number"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleChange(e)}
-        />
-        <TextField
-          name="totalBuildingSize"
-          label="Building Size"
-          type="text"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleChange(e)}
-        />
-        <TextField
-          name="dateBuilt"
-          slotProps={{ inputLabel: { shrink: true } }} 
-          label="Date Built"
-          type="date"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleChange(e)}
-        />
-        <TextField
-          name="numOfElevators"
-          label="Amount of Elevators"
-          type="number"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleChange(e)}
-        />
-        <TextField
-          name="streetNumber"
-          label="Street Number"
-          type="text"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleAddressChange(e)}
-        />
-        <TextField
-          name="streetName"
-          label="Street Name"
-          type="text"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleAddressChange(e)}
-        />
-        <TextField
-          name="district"
-          label="District"
-          type="text"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleAddressChange(e)}
-        />
-        <TextField
-          name="entrance"
-          label="Entrance"
-          type="text"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleAddressChange(e)}
-        />
-        <TextField
-          name="city"
-          label="City"
-          type="text"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleAddressChange(e)}
-        />
-        <TextField
-          name="postalCode"
-          label="Postal Code"
-          type="text"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleAddressChange(e)}
-        />
-        <TextField
-          name="Country"
-          label="Country"
-          type="text"
-          variant="outlined"
-          size="small"
-          fullWidth
-          onChange={(e) => handleAddressChange(e)}
-        />
-        <Button fullWidth type="submit">
-          {createBuildingConstants.create}
-        </Button>
-      </form>
+      <div className="text-field-container">
+        <form
+          className="create-building-form"
+          onSubmit={(e) => handleCreateBuildingSubmission(e)}
+        >
+          <h1 className="create-building-header">
+            {createBuildingConstants.createHeader}
+          </h1>
+          <TextField
+            name="alias"
+            label="Building Alias"
+            type="text"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleChange(e)}
+          />
+          <TextField
+            name="floorNum"
+            label="Amount of Building Floors"
+            type="number"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleChange(e)}
+          />
+          <TextField
+            name="totalBuildingSize"
+            label="Building Size"
+            type="text"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleChange(e)}
+          />
+          <TextField
+            name="dateBuilt"
+            slotProps={{ inputLabel: { shrink: true } }}
+            label="Date Built"
+            type="date"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleChange(e)}
+          />
+          <TextField
+            name="numOfElevators"
+            label="Amount of Elevators"
+            type="number"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleChange(e)}
+          />
+          <TextField
+            name="streetNumber"
+            label="Street Number"
+            type="text"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleAddressChange(e)}
+          />
+          <TextField
+            name="streetName"
+            label="Street Name"
+            type="text"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleAddressChange(e)}
+          />
+          <TextField
+            name="district"
+            label="District"
+            type="text"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleAddressChange(e)}
+          />
+          <TextField
+            name="entrance"
+            label="Entrance"
+            type="text"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleAddressChange(e)}
+          />
+          <TextField
+            name="city"
+            label="City"
+            type="text"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleAddressChange(e)}
+          />
+          <TextField
+            name="postalCode"
+            label="Postal Code"
+            type="text"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleAddressChange(e)}
+          />
+          <TextField
+            name="Country"
+            label="Country"
+            type="text"
+            variant="outlined"
+            size="small"
+            fullWidth
+            onChange={(e) => handleAddressChange(e)}
+          />
+          <Button fullWidth type="submit">
+            {createBuildingConstants.create}
+          </Button>
+        </form>
       </div>
       <div className="property-table">
+        <TextField
+          name="propertyNumber"
+          label="Property Number"
+          type="text"
+          variant="outlined"
+          size="small"
+          fullWidth
+          onChange={(e) => handlePropertyChange(e)}
+        />
         <Box>
-          <DataGrid 
+          <DataGrid
             rows={rows}
             columns={columns}
+            getRowId={(row) => row.propertyId}
             initialState={{
               pagination: {
                 paginationModel: {
@@ -195,10 +230,10 @@ const CreateBuildingPage: FC = () => {
             }}
             pageSizeOptions={[5]}
             checkboxSelection
-            />
+          />
         </Box>
-        </div>
       </div>
+    </div>
   );
 };
 export default CreateBuildingPage;
