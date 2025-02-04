7b8ac8b2ba0825ec2d92eef9ba002c7c7a70fed5
Kiril Syuleyman <kiril.syuleyman@blankfactor.com>
2025-02-04 11:41:20 +0200

    Updated repository and service so that building returns address,expenses and properties too. Updated automapper to match that.

--- NextGen-BM-BE/NextGen-BM-BE-Domain/Interfaces/ServiceInterfaces/IBuildingService.cs
+++ NextGen-BM-BE/NextGen-BM-BE-Domain/Interfaces/ServiceInterfaces/IBuildingService.cs
@@ -5,8 +5,8 @@ namespace NextGen_BM_BE_Domain.Interfaces.ServiceInterfaces
 {
     public interface IBuildingService
     {
-        Task<Building> GetBuildingByIdAsync(int buildingId);
-        Task<IList<Building>> GetAllBuildingsAsync();
+        Task<BuildingViewModel> GetBuildingByIdAsync(int buildingId);
+        Task<IList<BuildingViewModel>> GetAllBuildingsAsync();
         Task CreateBuildingAsync(BuildingViewModel building);
         Task UpdateBuildingAsync(BuildingViewModel building);
         Task DeleteBuildingAsync(int buildingId);
