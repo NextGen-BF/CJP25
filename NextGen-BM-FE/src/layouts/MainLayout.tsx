78f695cb73a3582600062a4e1c1b0abc8c808bd3
Kiril Syuleyman <kiril.syuleyman@blankfactor.com>
2025-02-05 11:48:39 +0200

    Added test logout button. Will be anchored in top navbar after it is created

--- NextGen-BM-FE/src/layouts/MainLayout.tsx
+++ NextGen-BM-FE/src/layouts/MainLayout.tsx
@@ -1,10 +1,18 @@
 import { FC } from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import { Button } from "@mui/material";
+import { useAppDispatch } from "../redux/store";
+import { logout } from "../redux/slices/loginSlice";
+import { logOut } from "../constants/constants";
 
 const MainLayout: FC = () => {
+  const dispatch = useAppDispatch();
   return (
     <>
+      <Button onClick={() => dispatch(logout())}>
+        {logOut.logout}
+      </Button>
       <Navbar />
       <Outlet />
     </>
--- NextGen-BM-FE/src/redux/store.ts
+++ NextGen-BM-FE/src/redux/store.ts
@@ -9,7 +9,6 @@ export const store =  configureStore({
         loginReducer: loginReducer,
         buildingReducer: buildingReducer,
         signupReducer: signupReducer,
-
     },
 })
 
