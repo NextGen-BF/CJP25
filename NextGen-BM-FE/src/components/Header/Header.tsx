d95b17bf011bbc0df1428dbc3424201f06ad7bf1
Anton Kostov <anton.kostov@blankfactor.com>
2025-02-05 10:49:47 +0200

    style changes and put page title in header

--- NextGen-BM-FE/src/components/Header/Header.tsx
+++ NextGen-BM-FE/src/components/Header/Header.tsx
@@ -1,16 +1,19 @@
 import { FC } from "react";
-import { NavLink } from "react-router-dom";
-import { AppBar } from "@mui/material";
+import { NavLink, useLocation } from "react-router-dom";
+import { AppBar, Button } from "@mui/material";
 import { headerStyles } from "./HeaderStyles.ts";
 import "./header.scss"
 import { store } from "../../redux/store.ts";
+import { pageTitles } from "../../constants/pageTitlesConstant.ts";
 
 export const Header: FC = () => {
     const userToken = store.getState().loginReducer.value;
+    const location=useLocation().pathname;
     return (
         <AppBar sx={headerStyles.appBarStyles}>
+            <Button>0</Button>
             <NavLink className={'header-link'} to="/">Home</NavLink>
-            <span className={'header-link'}>Title</span>
+            <span className={'header-link'}>{pageTitles[location]}</span>
             {/* todo: put logout here */}
             {userToken.accessToken?"":<NavLink className={'header-link'} to="/login">Log in</NavLink>}
         </AppBar>
---
+++ NextGen-BM-FE/src/layouts/MainLayoutStyles.ts
@@ -0,0 +1,16 @@
+import { SxProps } from "@mui/material";
+import { navbarStyles } from "../components/Navbar/NavbarStyles.ts";
+
+export const mainLayoutStyles:{[key:string]:SxProps}={
+    mainContainerStyle:{
+        display: 'flex',
+        flexDirection:'row',
+        flexWrap: 'wrap'
+    },
+    contentContainerStyle:{
+        display: 'flex',
+        flexDirection:'column',
+        flexWrap: 'wrap',
+        width:"80%"
+    }
+};
