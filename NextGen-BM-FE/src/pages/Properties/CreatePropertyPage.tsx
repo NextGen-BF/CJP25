b06976a4febb4e83ec32fec1839af548b83c5603
Anton Kostov <anton.kostov@blankfactor.com>
2025-02-06 18:08:39 +0200

    link fe w/ create property endpoint & init react hook form for create property page

--- NextGen-BM-FE/src/pages/Properties/CreatePropertyPage.tsx
+++ NextGen-BM-FE/src/pages/Properties/CreatePropertyPage.tsx
@@ -1,6 +1,39 @@
 import { FC } from "react";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { Property } from "../../models/property";
+import { useAppDispatch } from "../../redux/store";
+import { createProperty } from "../../redux/services/propertyService";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 
 const CreatePropertyPage: FC = () => {
-  return <h1>Create Property Page</h1>;
+  const dispatch = useAppDispatch();
+  const {control, handleSubmit} = useForm<Property>();
+  const onSubmit: SubmitHandler<Property> = (data) => dispatch(createProperty(data));
+
+  return (
+    <div>
+      <h1>Create Property Page</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="propertyNumber"
+          control={control}
+          render={({ field }) => (
+            <TextField
+            {...field}
+            label="Property Number"
+            type="number"
+            variant="outlined"
+            size="small"
+            fullWidth
+            />
+          )}
+        />
+        <Button type="submit">
+          Create Property
+        </Button>
+      </form>
+    </div>
+  );
 };
 export default CreatePropertyPage;
---
+++ NextGen-BM-FE/src/redux/services/propertyService.ts
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { Property } from "../../models/property";
+import axios, { AxiosError } from "axios";
+import { apiURL } from "../../api/shared";
+
+export const createProperty = createAsyncThunk('/property/new', async(property:Property)=>{
+    return await axios.post(`${apiURL}/property/new`, property)
+        .then(
+            function(response){
+                return response.data;
+            }
+        )
+        .catch((err: Error | AxiosError) => {
+            if(axios.isAxiosError(err))
+                return err.response;
+            return err;
+        })
+    }
+);
