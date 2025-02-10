de1ab077cc92ec6eb1fc52875ef3460afe14c283
IrinaAtanasova <irina.atanasova@blankfactor.com>
2025-02-10 11:52:46 +0200

    refactoring the service for creating property expense

--- NextGen-BM-FE/src/redux/services/expenseService.ts
+++ NextGen-BM-FE/src/redux/services/expenseService.ts
@@ -3,22 +3,15 @@ import axios, { AxiosError } from "axios";
 import { apiURL } from "../../api/shared";
 import { PropertyExpense } from "../../models/property";
 
-export const createPropertyExpense = createAsyncThunk(
-  "expense/new",
-  async (data: PropertyExpense, thunkAPI) => {
-    return await axios
-      .post(`${apiURL}/expense/new`, data)
-      .then(function (response) {
-        return {
-          data: response.data,
-          status: response.status,
-          statusText: response.statusText,
-        };
-      })
-      .catch((err: Error | AxiosError) => {
-        if (axios.isAxiosError(err))
-          return thunkAPI.rejectWithValue(err.response?.data);
-        return thunkAPI.rejectWithValue(err);
-      });
-  },
-);
+export const createPropertyExpense=createAsyncThunk("expense/new", async(expense: PropertyExpense)=> {
+  return await axios.post(`${apiURL}/expense/new`, expense, {headers: {
+      Authorization: `Bearer ${localStorage.getItem("JWT-BM")}`
+  }})
+  .then(function(response){
+      return response.data;
+  }).catch((err: Error | AxiosError) => {
+      if(axios.isAxiosError(err))
+          return err.response
+      return err;
+  })
+})
