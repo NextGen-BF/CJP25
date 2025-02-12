a3c9f52b4cbf85a986193a0c5a2ef8b8ec40639c
IrinaAtanasova <irina.atanasova@blankfactor.com>
2025-02-12 15:42:18 +0200

    making changes so that the form is not dependent on another page

--- NextGen-BM-FE/src/pages/Properties/PropertyFeesPage.tsx
+++ NextGen-BM-FE/src/pages/Properties/PropertyFeesPage.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@mui/material";
 import { FC } from "react";
-import { NavLink } from "react-router-dom";
 
 const PropertyFeesPage: FC = () => {
   return (
     <>
       <h1>Apartment Fees Page</h1>
-      <Button variant="contained"><NavLink style={{ textDecoration: 'none', color: 'white' }} to="/create/propertyexpense">Create Property Expense</NavLink></Button>
     </>
   );
 };
