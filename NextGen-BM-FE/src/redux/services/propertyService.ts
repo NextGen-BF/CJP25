5d6f2166a6bbacf1d85b2b65e0d9550bebebcac0
Anton Kostov <anton.kostov@blankfactor.com>
2025-02-12 11:26:07 +0200

    fix http header typo

--- NextGen-BM-FE/src/redux/services/propertyService.ts
+++ NextGen-BM-FE/src/redux/services/propertyService.ts
@@ -6,7 +6,7 @@ import { apiURL } from "../../api/shared";
 export const createProperty = createAsyncThunk('/property/new', async(property:Property)=>{
     return await axios.post(`${apiURL}/property/new`, property, {
         headers: 
-            {Authorization: `Bearer: ${localStorage.getItem('JWT-BM')}`}
+            {Authorization: `Bearer ${localStorage.getItem('JWT-BM')}`}
         })
         .then(
             function(response){
