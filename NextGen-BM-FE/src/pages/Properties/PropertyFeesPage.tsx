3bd978c9a19693a02f7e42058d43b9dd11f43e75
IrinaAtanasova <irina.atanasova@blankfactor.com>
2025-02-12 16:58:42 +0200

    removing unnecessary code

--- NextGen-BM-FE/src/pages/Properties/PropertyFeesPage.tsx
+++ NextGen-BM-FE/src/pages/Properties/PropertyFeesPage.tsx
@@ -3,20 +3,13 @@ import {
   Button,
   IconButton,
   InputAdornment,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   TextField,
   Typography,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { FC, useState } from "react";
 import "./propertyFeesPage.scss";
-import { PropertyExpense, PropertyPayments } from "../../models/property";
-import Paper from "@mui/material/Paper";
+import { PropertyPayments } from "../../models/property";
 import { DataGrid } from "@mui/x-data-grid";
 import { NavLink } from "react-router-dom";
 
@@ -85,40 +78,7 @@ const PropertyFeesPage: FC = () => {
       flex: 1,
     },
   ];
-  const propertyExpensesMockData: PropertyExpense[] = [
-    {
-      propertyExpenseId: 1,
-      propertyExpenseTemplateId: 1,
-      responsibleRole: "owner",
-      price: 222.4,
-      startDate: new Date("2024-12-20"),
-      endDate: new Date("2025-01-11"),
-      Description: "electricity bill",
-    },
-    {
-      propertyExpenseId: 2,
-      propertyExpenseTemplateId: 1,
-      responsibleRole: "tenant",
-      price: 52.55,
-      startDate: new Date("2025-01-29"),
-      endDate: new Date("2025-03-01"),
-      Description: "water bill",
-    },
-    {
-      propertyExpenseId: 3,
-      propertyExpenseTemplateId: 1,
-      responsibleRole: "tenant",
-      price: 22.0,
-      startDate: new Date("2025-01-02"),
-      endDate: new Date("2025-03-12"),
-      Description: "monthly rent",
-    },
-  ];
-
-  const [propertyExpenses, setPropertyExpenses] = useState<PropertyExpense[]>(
-    propertyExpensesMockData,
-  );
-
+  
   const [propertyPayments, setPropertyPayments] = useState<PropertyPayments[]>(
     propertyPaymentsMockData,
   );
@@ -127,20 +87,20 @@ const PropertyFeesPage: FC = () => {
 
   const handleSearch = () => {
     if (searchInput !== "") {
-      const filteredPropertyExpenses = propertyExpenses.filter(
-        (propertyExpense) => {
+      const filteredPropertyPayments = propertyPayments.filter(
+        (propertyPayment) => {
           if (
-            propertyExpense.Description.toLowerCase().includes(
+            propertyPayment.paymentMethod.toLowerCase().includes(
               searchInput.toLowerCase(),
             )
           ) {
-            return propertyExpense;
+            return propertyPayment;
           }
         },
       );
-      setPropertyExpenses(filteredPropertyExpenses);
+      setPropertyPayments(filteredPropertyPayments);
     } else {
-      setPropertyExpenses(propertyExpensesMockData);
+      setPropertyPayments(propertyPaymentsMockData);
       return;
     }
   };
@@ -148,12 +108,6 @@ const PropertyFeesPage: FC = () => {
   return (
     <>
       <h1>Apartment Fees Page</h1>
-
-      <DataGrid
-        rows={propertyPayments}
-        columns={columns}
-        getRowId={(row) => row.paymentId}
-      />
 
       <div className="button-container">
         <Button
@@ -210,38 +164,11 @@ const PropertyFeesPage: FC = () => {
         />
       </Box>
 
-      <TableContainer component={Paper}>
-        <Table sx={{ width: "100%" }} size="small" aria-label="a dense table">
-          <TableHead sx={{ "& .MuiTableCell-root": { fontWeight: "bold" } }}>
-            <TableRow>
-              <TableCell align="center">Description</TableCell>
-              <TableCell align="center">Start Date</TableCell>
-              <TableCell align="center">End Date</TableCell>
-              <TableCell align="center">Price</TableCell>
-              <TableCell align="center">Role</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {propertyExpenses.map((propertyExpense) => (
-              <TableRow hover key={propertyExpense.propertyExpenseId}>
-                <TableCell align="center">
-                  {propertyExpense.Description}
-                </TableCell>
-                <TableCell align="center">
-                  {formatDate(propertyExpense.startDate)}
-                </TableCell>
-                <TableCell align="center">
-                  {formatDate(propertyExpense.endDate)}
-                </TableCell>
-                <TableCell align="center">{propertyExpense.price}</TableCell>
-                <TableCell align="center">
-                  {propertyExpense.responsibleRole}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <DataGrid
+        rows={propertyPayments}
+        columns={columns}
+        getRowId={(row) => row.paymentId}
+      />
     </>
   );
 };
