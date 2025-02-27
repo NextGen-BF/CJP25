9825e0aec716c5e6e18f288c0862000b57b16420
IrinaAtanasova <irina.atanasova@blankfactor.com>
2025-02-27 09:58:56 +0200

    fixing the property used in find

--- NextGen-BM-FE/src/pages/Properties/CreatePropertyPaymentsPage.tsx
+++ NextGen-BM-FE/src/pages/Properties/CreatePropertyPaymentsPage.tsx
@@ -103,7 +103,7 @@ const CreatePropertyPaymentsPage: FC = () => {
   const dateOpened = watch("dateOpened");
 
   const onSubmit: SubmitHandler<PropertyPayments> = async (data) => {
-    data.propertyId = buildingProperties.find(p => p.propertyNumber == selectedProperty)?.propertyId || 0;
+    data.propertyId = buildingProperties.find(p => p.propertyId == selectedProperty)?.propertyId || 0;
     try {
       await dispatch(createPropertyPayment(data)).unwrap();
       dispatch(
